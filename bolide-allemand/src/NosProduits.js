99329317ea82f66266ed74e5c0c523710637c624
Sweeefty <108686673+Sweeefty@users.noreply.github.com>
2023-10-30 18:23:43 +0100

    adding card products

--- bolide-allemand/src/NosProduits.js
+++ bolide-allemand/src/NosProduits.js
@@ -1,11 +1,159 @@
 import React from 'react';
+import './index.css';
 
 function NosProduits() {
   return (
-    <div>
-      <h1>Nos Produits</h1>
-      <p>BONJOUR</p>
-    </div>
+	<div className='w-[100%] h-[100%] full-page'>
+		<div className="grid grid-cols-3 grid-rows-4 gap-10">
+			<div className="card w-96 bg-base-100 shadow-xl rounded-lg">
+				<figure>
+					<img src="./bmw.jpg" alt="Shoes" />
+				</figure>
+				<div className="card-body bg!">
+					<h2 className="justify-center">Voiture</h2>
+					<p>voiture puissance</p>
+					<div className="p-2 bg-[#B3B3B3] justify-end">
+						<button className="btn btn-primary items-end "> Louer </button>
+					</div>
+				</div>	
+			</div>
+			<div className="card w-96 bg-base-100 shadow-xl rounded-lg">
+				<figure>
+					<img src="./bmw.jpg" alt="Shoes" />
+				</figure>
+				<div className="card-body bg!">
+					<h2 className="card-title">Voiture</h2>
+					<p>voiture puissance</p>
+					<div className="p-2 bg-[#B3B3B3] justify-end">
+						<button className="btn btn-primary"> Louer </button>
+					</div>
+				</div>	
+			</div>
+			<div className="card w-96 bg-base-100 shadow-xl rounded-lg">
+				<figure>
+					<img src="./bmw.jpg" alt="Shoes" />
+				</figure>
+				<div className="card-body bg!">
+					<h2 className="card-title">Voiture</h2>
+					<p>voiture puissance</p>
+					<div className="p-2 bg-[#B3B3B3] justify-end">
+						<button className="btn btn-primary"> Louer </button>
+					</div>
+				</div>	
+			</div>
+			<div className="card w-96 bg-base-100 shadow-xl rounded-lg">
+				<figure>
+					<img src="./bmw.jpg" alt="Shoes" />
+				</figure>
+				<div className="card-body bg!">
+					<h2 className="card-title">Voiture</h2>
+					<p>voiture puissance</p>
+					<div className="p-2 bg-[#B3B3B3] justify-end">
+						<button className="btn btn-primary"> Louer </button>
+					</div>
+				</div>	
+			</div>
+			<div className="card w-96 bg-base-100 shadow-xl rounded-lg">
+				<figure>
+					<img src="./bmw.jpg" alt="Shoes" />
+				</figure>
+				<div className="card-body bg!">
+					<h2 className="card-title">Voiture</h2>
+					<p>voiture puissance</p>
+					<div className="p-2 bg-[#B3B3B3] justify-end">
+						<button className="btn btn-primary"> Louer </button>
+					</div>
+				</div>	
+			</div>
+			<div className="card w-96 bg-base-100 shadow-xl rounded-lg">
+				<figure>
+					<img src="./bmw.jpg" alt="Shoes" />
+				</figure>
+				<div className="card-body bg!">
+					<h2 className="card-title">Voiture</h2>
+					<p>voiture puissance</p>
+					<div className="p-2 bg-[#B3B3B3] justify-end">
+						<button className="btn btn-primary"> Louer </button>
+					</div>
+				</div>	
+			</div>
+			<div className="card w-96 bg-base-100 shadow-xl rounded-lg">
+				<figure>
+					<img src="./bmw.jpg" alt="Shoes" />
+				</figure>
+				<div className="card-body bg!">
+					<h2 className="card-title">Voiture</h2>
+					<p>voiture puissance</p>
+					<div className="p-2 bg-[#B3B3B3] justify-end">
+						<button className="btn btn-primary"> Louer </button>
+					</div>
+				</div>	
+			</div>
+			<div className="card w-96 bg-base-100 shadow-xl rounded-lg">
+				<figure>
+					<img src="./bmw.jpg" alt="Shoes" />
+				</figure>
+				<div className="card-body bg!">
+					<h2 className="card-title">Voiture</h2>
+					<p>voiture puissance</p>
+					<div className="p-2 bg-[#B3B3B3] justify-end">
+						<button className="btn btn-primary"> Louer </button>
+					</div>
+				</div>	
+			</div>
+			<div className="card w-96 bg-base-100 shadow-xl rounded-lg">
+				<figure>
+					<img src="./bmw.jpg" alt="Shoes" />
+				</figure>
+				<div className="card-body bg!">
+					<h2 className="card-title">Voiture</h2>
+					<p>voiture puissance</p>
+					<div className="p-2 bg-[#B3B3B3] justify-end">
+						<button className="btn btn-primary"> Louer </button>
+					</div>
+				</div>	
+			</div>
+			<div className="card w-96 bg-base-100 shadow-xl rounded-lg">
+				<figure>
+					<img src="./bmw.jpg" alt="Shoes" />
+				</figure>
+				<div className="card-body bg!">
+					<h2 className="card-title">Voiture</h2>
+					<p>voiture puissance</p>
+					<div className="p-2 bg-[#B3B3B3] justify-end">
+						<button className="btn btn-primary"> Louer </button>
+					</div>
+				</div>	
+			</div>
+			<div className="card w-96 bg-base-100 shadow-xl rounded-lg">
+				<figure>
+					<img src="./bmw.jpg" alt="Shoes" />
+				</figure>
+				<div className="card-body bg!">
+					<h2 className="card-title">Voiture</h2>
+					<p>voiture puissance</p>
+					<div className="p-2 bg-[#B3B3B3] justify-end">
+						<button className="btn btn-primary"> Louer </button>
+					</div>
+				</div>	
+			</div>
+			<div className="card w-96 bg-base-100 shadow-xl rounded-lg">
+				<figure>
+					<img src="./bmw.jpg" alt="Shoes" />
+				</figure>
+				<div className="card-body bg!">
+					<h2 className="card-title">Voiture</h2>
+					<p>voiture puissance</p>
+					<div className="p-2 bg-[#B3B3B3] justify-end">
+						<button className="btn btn-primary"> Louer </button>
+					</div>
+				</div>	
+			</div>
+
+		</div>
+	</div>
+
+
   );
 }
 
--- bolide-allemand/tailwind.config.js
+++ bolide-allemand/tailwind.config.js
@@ -1,9 +1,43 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: [
+	  './src/**/*.{js,jsx,ts,tsx}',
+	],
+	theme: {
+	  extend: {},
+	},
+	plugins: [],
+		//...
+		daisyui: {
+		  themes: [
+			"light",
+			"dark",
+			"cupcake",
+			"bumblebee",
+			"emerald",
+			"corporate",
+			"synthwave",
+			"retro",
+			"cyberpunk",
+			"valentine",
+			"halloween",
+			"garden",
+			"forest",
+			"aqua",
+			"lofi",
+			"pastel",
+			"fantasy",
+			"wireframe",
+			"black",
+			"luxury",
+			"dracula",
+			"cmyk",
+			"autumn",
+			"business",
+			"acid",
+			"lemonade",
+			"night",
+			"coffee",
+			"winter",
+		  ],
+	  }
+  }
