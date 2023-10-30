d4e5dc529d0a2ab2a63076b146ec0da81550621e
Sweeefty <108686673+Sweeefty@users.noreply.github.com>
2023-10-30 19:14:16 +0100

    fix header

--- bolide-allemand/src/header.js
+++ bolide-allemand/src/header.js
@@ -1,18 +1,21 @@
-// src/Header.js
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <header style={{ backgroundColor: 'gray' }}>
-      <nav>
-        <ul>
-          <li><Link to="/">Accueil</Link></li>
-          <li><Link to="/apropos">À Propos</Link></li>
-          <li><Link to="/nosproduits">Nos Produits</Link></li>
-        </ul>
-      </nav>
-    </header>
+    <nav className="bg-gray-300 p-4 flex justify-center">
+      <ul className="list-none flex gap-4">
+        <li>
+          <Link to="/" className="text-black-500 hover:text-black-700">Accueil</Link>
+        </li>
+        <li>
+          <Link to="/apropos" className="text-black-500 hover:text-black-700">À Propos</Link>
+        </li>
+        <li>
+          <Link to="/nosproduits" className="text-black-500 hover:text-black-700">Nos Produits</Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
