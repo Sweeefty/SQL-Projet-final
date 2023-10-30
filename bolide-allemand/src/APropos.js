692b570bf42ca1cf235c377317a0101745e54c7b
Sweeefty <108686673+Sweeefty@users.noreply.github.com>
2023-10-30 16:32:09 +0100

    adding css

--- bolide-allemand/src/APropos.js
+++ bolide-allemand/src/APropos.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './APropos.css';
 
 function APropos() {
   return (
--- bolide-allemand/src/NosProduits.js
+++ bolide-allemand/src/NosProduits.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import './NosProduits.css';
+
 
 function NosProduits() {
   return (
