b66b4c26162e9884e6f0b0cba17880016038927f
Sweeefty <108686673+Sweeefty@users.noreply.github.com>
2023-10-30 19:52:13 +0100

    fix header and adding connexion page with forms

--- bolide-allemand/src/header.js
+++ bolide-allemand/src/header.js
@@ -3,18 +3,22 @@ import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <nav className="bg-gray-300 p-4 flex justify-center">
+    <nav className="bg-gray-300 p-4 flex justify-between">
       <ul className="list-none flex gap-4">
         <li>
-          <Link to="/" className="text-black-500 hover:text-black-700">Accueil</Link>
+          <Link to="/" className="text-gray-800">Accueil</Link>
         </li>
         <li>
-          <Link to="/apropos" className="text-black-500 hover:text-black-700">À Propos</Link>
+          <Link to="/apropos" className="text-gray-800 text-l">À Propos</Link>
         </li>
         <li>
-          <Link to="/nosproduits" className="text-black-500 hover:text-black-700">Nos Produits</Link>
+          <Link to="/nosproduits" className="text-gray-800">Nos Produits</Link>
         </li>
       </ul>
+      <div className="flex gap-4">
+        <Link to="/inscription" className="text-gray-800">Inscription</Link>
+        <Link to="/connexion" className="text-gray-800">Connexion</Link>
+      </div>
     </nav>
   );
 }
