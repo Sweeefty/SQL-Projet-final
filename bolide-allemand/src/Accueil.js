01c045f4ea1f4e7f56174b36ab9c09a5f68c3090
Sweeefty <108686673+Sweeefty@users.noreply.github.com>
2023-10-30 16:04:59 +0100

    navigation entre les pages fixed

--- bolide-allemand/src/Accueil.js
+++ bolide-allemand/src/Accueil.js
@@ -1,9 +1,10 @@
 // src/Accueil.js
 import React from 'react';
+import './Accueil.css';
 
 function Accueil() {
   return (
-	
+
     <div>
       <h1>Page d'accueil de BOLIDE ALLEMAND</h1>
 	  <span>La vrai location de luxe</span>
--- bolide-allemand/src/App.js
+++ bolide-allemand/src/App.js
@@ -1,22 +1,19 @@
-// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './header';
 import Accueil from './Accueil';
 import APropos from './APropos';
-import NosProduits from './NosProduits.js';
+import NosProduits from './NosProduits';
+import Header from './header';
 
 function App() {
   return (
     <Router>
-      <div>
-        <Header />
-        <Routes>
-          <Route exact path="/" component={Accueil} />
-          <Route path="/apropos" component={APropos} />
-          <Route path="/nosproduits" component={NosProduits} />
-        </Routes>
-      </div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Accueil />} />
+        <Route path="/apropos" element={<APropos />} />
+        <Route path="/nosproduits" element={<NosProduits />} />
+      </Routes>
     </Router>
   );
 }
