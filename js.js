cb36659859ecef8496238635754f91333d344824
anas rayk <anasrayk@MacBook-Air-de-anas.local>
2023-10-30 07:52:18 +0100

    pull

--- js.js
+++
@@ -1,9 +0,0 @@
-const title = document.querySelector("h1");
-
-title.addEventListener("mouseover", function() {
-    title.style.backgroundColor = "#FF5722"; 
-});
-
-title.addEventListener("mouseout", function() {
-    title.style.backgroundColor = "transparent"; 
-});
